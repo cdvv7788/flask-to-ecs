4cacf355a3cc880e7c0875b2b1ef484f1a838b62
Cristian <cristian@swapps.com>
2020-11-22 15:43:36 -0500

    feat: add unittest

---
+++ app/tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestApp(unittest.TestCase):
+
+    def test_something(self):
+        """This is just a placeholder"""
+        self.assertTrue(1)
+
+if __name__ == "__main__":
+    unittest.main()
